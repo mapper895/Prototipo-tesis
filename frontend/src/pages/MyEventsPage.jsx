008ef4d31ae01d27e91b8a5394cecf5f9f5ba163
mapper895 <mapper.895.mp@gmail.com>
2025-03-31 21:32:50 -0600

    Modificacion de MyEventsPage para botones de modificar y eliminar eventos - Solo se ha implementado la funcion de modificar

--- frontend/src/pages/MyEventsPage.jsx
+++ frontend/src/pages/MyEventsPage.jsx
@@ -4,7 +4,13 @@ import { useAuthStore } from "../store/authUser";
 import { useEventStore } from "../store/eventStore";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
-import { Calendar, ChevronDown, SlidersHorizontal } from "lucide-react";
+import {
+  Calendar,
+  ChevronDown,
+  SlidersHorizontal,
+  Pencil,
+  Trash2,
+} from "lucide-react";
 
 const MyEventsPage = () => {
   const { user } = useAuthStore();
@@ -57,28 +63,40 @@ const MyEventsPage = () => {
 
         <div className="grid grid-cols-4 gap-7 my-5">
           {events.map((event) => (
-            <Link
+            <div
+              className="flex flex-col shadow-lg rounded-lg overflow-hidden"
               key={event._id}
-              className="shadow-lg rounded-lg overflow-hidden "
-              to={`/events/${event._id}`}
             >
-              <img
-                src={event.imageUrl}
-                alt="evento"
-                className="w-full h-48 object-cover hover:scale-110 transition-transform duration-300 ease-in-out"
-              />
-              <div className="font-bold text-lg ml-2 mt-2">{event.title}</div>
-              <div className="text-gray-600 mx-2 mb-1">
-                {event.description.length > 60
-                  ? `${event.description.slice(0, 55)}...`
-                  : event.description}{" "}
-                {event.description.length > 60 && (
-                  <span className="text-blue-600 hover:underline mt-2">
-                    Ver más
-                  </span>
-                )}
+              <Link to={`/events/${event._id}`}>
+                <img
+                  src={event.imageUrl}
+                  alt="evento"
+                  className="w-full h-48 object-cover hover:scale-110 transition-transform duration-300 ease-in-out"
+                />
+                <div className="font-bold text-lg ml-2 mt-2">{event.title}</div>
+                <div className="text-gray-600 mx-2 mb-1">
+                  {event.description.length > 60
+                    ? `${event.description.slice(0, 55)}...`
+                    : event.description}{" "}
+                  {event.description.length > 60 && (
+                    <span className="text-blue-600 hover:underline mt-2">
+                      Ver más
+                    </span>
+                  )}
+                </div>
+              </Link>
+              <div className="flex">
+                <Link
+                  className="w-1/2 p-2 bg-blue-200 flex items-center justify-center gap-2 text-sm"
+                  to={`/edit-event/${event._id}`}
+                >
+                  Editar evento <Pencil size={16} />
+                </Link>
+                <Link className="w-1/2 p-2 bg-red-500 flex items-center justify-center gap-2 text-sm">
+                  Eliminar evento <Trash2 size={16} />
+                </Link>
               </div>
-            </Link>
+            </div>
           ))}
         </div>
       </div>
