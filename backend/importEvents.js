84b75ea61fc2f6130712204f7a7a6ba5802f5cec
mapper895 <mapper.895.mp@gmail.com>
2025-05-08 12:39:15 -0600

    Funcion para subir los eventos obtenidos mediante web scraping a la base de datos

---
+++ backend/importEvents.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import fs from "fs";
+
+const API_URL = "http://localhost:5000/api/v1/event/create-event";
+
+fs.readFile("events_29_04_2025.json", "utf8", async (error, data) => {
+  if (error) {
+    console.log("Error al leer el archivo: ", error);
+    return;
+  }
+
+  const events = JSON.parse(data);
+
+  const createEvent = async (event) => {
+    try {
+      const reponse = await axios.post(API_URL, event);
+      console.log(`Evento creado: ${event.title}`);
+    } catch (error) {
+      console.log(
+        `Error al crear el evento ${event.title}: `,
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+    await createEvent(event);
+  }
+
+  console.log("Todos los eventos han sido procesados");
+});
