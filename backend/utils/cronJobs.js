f8c68e2495977dbbdf1889ee9ad1e5d2f90771c4
mapper895 <mapper.895.mp@gmail.com>
2025-05-15 15:43:34 -0600

    Uso de node-cron, libreria que nos ayuda a programar funciones, en este caso, el envio de correos segun el dia y la hora

--- backend/utils/cronJobs.js
+++ backend/utils/cronJobs.js
@@ -1,15 +1,54 @@
 import cron from "node-cron";
+import { User } from "../models/user.model.js";
 import {
-  createEventReminderNotifications,
-  createSimilarEventNotifications,
+  sendWeeklyEvents,
+  getWeeklyFeaturedEvents,
+  notifyReservationsForTomorrow,
+  notifyEventEnded,
 } from "./notificationService.js";
 
-export const scheduleNotification = () => {
-  // Programar la tarea para enviar recordatorios de eventos próximos todos los días a medianoche
-  cron.schedule("0 0 * * *", createEventReminderNotifications); // Ejecuta todos los días a las 00:00
+// Envia los eventos destacados de la semana
+cron.schedule("0 8 * * 2", async () => {
+  // Cada martes a las 8am
+  try {
+    const events = await getWeeklyFeaturedEvents();
 
-  // Programar la tarea para enviar notificaciones de eventos similares (puedes cambiar la frecuencia de la tarea según lo necesites)
-  cron.schedule("0 0 * * *", createSimilarEventNotifications); // Ejecuta todos los días a las 00:00
+    if (events.length === 0) {
+      console.log("No hay eventos destacados esta semana.");
+      return;
+    }
 
-  createEventReminderNotifications();
-};
+    const users = await User.find();
+
+    for (const user of users) {
+      await sendWeeklyEvents(user.email, events);
+    }
+
+    console.log("Correos semanales enviados correctamente");
+  } catch (error) {
+    console.log("Error en cron job de eventos destacados: ", error);
+  }
+});
+
+// Obtiene y envia notificaciones sobre las reservas de los eventos que son el dia de mañana
+cron.schedule("0 8 * * *", async () => {
+  //Diaramente a las 8AM
+  console.log("Ejecutando cron para notificar reservas de eventos de mañana");
+  try {
+    await notifyReservationsForTomorrow();
+    console.log("Notificaciones y correos enviados correctamente");
+  } catch (error) {
+    console.log("Error enviando notificaciones");
+  }
+});
+
+// Envia correos notificando eventos terminados
+cron.schedule("0 9 * * *", async () => {
+  console.log("Ejecutando cron para notificar eventos terminados");
+  try {
+    await notifyEventEnded();
+    console.log("Correos de eventos terminados enviados");
+  } catch (error) {
+    console.log("Error notificando eventos terminados");
+  }
+});
