539a494361f9943ad3c2f17577f9ff4774bf986c
mapper895 <mapper.895.mp@gmail.com>
2025-05-15 15:48:41 -0600

    Se envia un correo de bienvenida al crear un usuario

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateToken.js";
+import { sendWelcomeEmail } from "../utils/notificationService.js";
 
 export async function signup(req, res) {
   try {
@@ -62,6 +63,8 @@ export async function signup(req, res) {
 
     generateTokenAndSetCookie(newUser._id, res);
     await newUser.save();
+
+    await sendWelcomeEmail(email);
 
     res.status(201).json({
       success: true,
