ceb486c23f2a317e767fa571b898c10aada7adfc
mapper895 <mapper.895.mp@gmail.com>
2025-06-09 20:46:36 -0600

    Not Found Page ahora es responsive

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,16 +1,31 @@
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import SmallNavbar from "../components/SmallNavbar";
 
 const NotFoundPage = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Detectar el tamaño de la pantalla
+  const checkScreenSize = () => {
+    setIsMobile(window.innerWidth <= 1280); // Consideramos 768px o menos como pantallas pequeñas
+  };
+
+  useEffect(() => {
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize); // Escuchar cambios de tamaño
+
+    return () => window.removeEventListener("resize", checkScreenSize); // Limpiar el evento
+  }, []);
   return (
     <>
-      <Navbar />
-      <div className="w-full mt-32 flex flex-col justify-center items-center gap-10">
-        <div className="w-[600px] h-[250px] justify-around flex items-center flex-col">
-          <div className="text-4xl text-center font-bold">
+      {isMobile ? <SmallNavbar /> : <Navbar />}
+      <div className="w-full h-screen flex flex-col justify-center items-center gap-10 px-4">
+        <div className="md:w-[600px] h-[250px] w-full justify-around flex items-center flex-col">
+          <div className="md:text-4xl text-2xl text-center font-bold">
             No encontramos esta pagina
           </div>
-          <div className="text-2xl text-center">
+          <div className="md:text-2xl text-lg text-center">
             Buscamos por todos lados, pero no encontramos la pagina que estas
             buscando
           </div>
