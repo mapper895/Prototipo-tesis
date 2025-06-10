6e16d9a030b24c5f5e04bab457eaab21eb8caa06
mapper895 <mapper.895.mp@gmail.com>
2025-06-09 20:32:20 -0600

    My Liked Events ahora es responsive

--- frontend/src/pages/MyLikedEvents.jsx
+++ frontend/src/pages/MyLikedEvents.jsx
@@ -8,6 +8,7 @@ import EventCard from "../components/EventCard";
 import EventFilters from "../components/EventFilters";
 import { filterEvents } from "../utils/filterEvents";
 import { Loader } from "lucide-react";
+import SmallNavbar from "../components/SmallNavbar";
 
 const MyLikedEvents = () => {
   const { user } = useAuthStore();
@@ -22,6 +23,19 @@ const MyLikedEvents = () => {
   const [selectedDate, setSelectedDate] = useState(null);
 
   const filteredEvents = filterEvents(eventUserLikes, filter, selectedDate);
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Detectar el tamaño de la pantalla
+  const checkScreenSize = () => {
+    setIsMobile(window.innerWidth <= 1280); // Consideramos 768px o menos como pantallas pequeñas
+  };
+
+  useEffect(() => {
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize); // Escuchar cambios de tamaño
+
+    return () => window.removeEventListener("resize", checkScreenSize); // Limpiar el evento
+  }, []);
 
   useEffect(() => {
     if (user) {
@@ -49,15 +63,15 @@ const MyLikedEvents = () => {
 
   return (
     <>
-      <Navbar />
-      <div className="max-w-[1300px] mx-auto mt-20">
+      {isMobile ? <SmallNavbar /> : <Navbar />}
+      <div className="max-w-screen-xl mx-auto xl:mt-20 px-4 xl:px-0 mb-10">
         <div className="flex flex-col gap-5 my-5">
-          <div className="flex gap-2">
+          <div className="flex gap-2 xl:text-base text-xs">
             <Link to="/">Inicio</Link>
             {">"}
             <p>Mis Likes</p>
           </div>
-          <h2 className="text-6xl font-light">Mis Likes</h2>
+          <h2 className="md:text-6xl text-4xl font-light">Mis Likes</h2>
         </div>
 
         <EventFilters
@@ -72,7 +86,7 @@ const MyLikedEvents = () => {
             <p className="text-xl font-semibold">No se encontraron eventos.</p>
           </div>
         ) : (
-          <div className="grid grid-cols-4 gap-7 my-5">
+          <div className="grid md:grid-cols-3 xl:grid-cols-4 grid-cols-2 md:gap-7 gap-4 md:my-5 my-2">
             {filteredEvents.map((event) => (
               <EventCard key={event._id} event={event} />
             ))}
