d716d3cf4ef567ada01557c7f5fb8a2af7d81fc7
mapper895 <mapper.895.mp@gmail.com>
2025-06-09 20:55:20 -0600

    Popular Page ahora es responsive

--- frontend/src/pages/PopularPage.jsx
+++ frontend/src/pages/PopularPage.jsx
@@ -6,6 +6,7 @@ import EventFilters from "../components/EventFilters";
 import EventCard from "../components/EventCard";
 import { filterEvents } from "../utils/filterEvents";
 import { Loader } from "lucide-react";
+import SmallNavbar from "../components/SmallNavbar";
 
 const PopularPage = () => {
   const { popularEvents, getPopularEvents, isLoadingEvents } = useEventStore();
@@ -13,6 +14,19 @@ const PopularPage = () => {
   const [selectedDate, setSelectedDate] = useState(null);
 
   const filteredEvents = filterEvents(popularEvents, filter, selectedDate);
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Detectar el tamaño de la pantalla
+  const checkScreenSize = () => {
+    setIsMobile(window.innerWidth <= 1280); // Consideramos 768px o menos como pantallas pequeñas
+  };
+
+  useEffect(() => {
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize); // Escuchar cambios de tamaño
+
+    return () => window.removeEventListener("resize", checkScreenSize); // Limpiar el evento
+  }, []);
 
   useEffect(() => {
     if (popularEvents.length === 0) {
@@ -31,11 +45,11 @@ const PopularPage = () => {
 
   return (
     <>
-      <Navbar />
-      <div className="max-w-[1300px] mx-auto mt-20">
+      {isMobile ? <SmallNavbar /> : <Navbar />}
+      <div className="max-w-screen-xl mx-auto xl:mt-20 px-4 xl:px-0 mb-10">
         <div className="flex flex-col gap-5 my-5">
-          <h2 className="text-6xl font-light">Top 10 CDMX</h2>
-          <p className="text-lg">
+          <h2 className="md:text-6xl text-4xl">Top 10 CDMX</h2>
+          <p className="xl:text-lg text-sm">
             Descubre los 10 mejores eventos en la Ciudad de México.
           </p>
         </div>
@@ -47,7 +61,7 @@ const PopularPage = () => {
           setSelectedDate={setSelectedDate}
         />
 
-        <div className="grid grid-cols-4 gap-7 my-5">
+        <div className="grid md:grid-cols-3 xl:grid-cols-4 grid-cols-2 md:gap-7 gap-4 md:my-5 my-2">
           {filteredEvents.map((event) => (
             <EventCard key={event._id} event={event} />
           ))}
